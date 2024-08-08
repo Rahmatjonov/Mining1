30021f63282370e7669ead177d32ee84a90069d8
Ubaydullo Rahmatjonov <145487224+Rahmatjonov@users.noreply.github.com>
2024-08-09 03:08:41 +0500

    Update and rename Mining to corner.sh

---
+++ corner.sh
@@ -0,0 +1,3 @@
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.48/lolMiner_v1.48_Lin64.tar.gz
+! tar -xf lolMiner_v1.48_Lin64.tar.gz
+! 1.48/lolMiner --algo ETHASH --pool ethash.unmineable.com:13333 --user BTC:bc1qw2ue55kknr2v0utd9ehdrwqf6pj4f0a8vzrs4h.neevBTC1#hvfd-qz5d --ethstratum ETHPROXY pause
